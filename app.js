2fbb9a32a746046ff896d4564babebf2ae3eab69
jemis140 <jemiszadafiya@gmail.com>
2020-09-02 11:53:48 +0530

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const errorController = require('./controllers/error');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-server.listen(3000);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
